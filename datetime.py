2e5ec194846a0a603c0aaaa23df16173238bcc64
Gaurav155s <gs4359359@gmail.com>
2024-07-16 12:13:48 +0530

    added 2-7 progresspf

--- datetime.py
+++ datetime.py
@@ -1,15 +1,55 @@
-import random
-cn=random.randrange(1,50)
-userinput=int(input("enter no:"))
+# import random
+# cn=random.randrange(1,50)
+# userinput=int(input("enter no:"))
+#
+# if cn>userinput:
+#     print(cn)
+#     print("cn is gretaer than you no.")
+#
+# elif userinput>cn:
+#     print(cn)
+#     print("user input greter than cn no")
+#
+# else:
+#     print("both no. are equal")
+#
 
-if cn>userinput:
-    print(cn)
-    print("cn is gretaer than you no.")
+# x=int(input("enter no."))
+# y=int(input("enter 2nd no.:"))
+#
+# if x>y:
+#     print(x,"x greter than y", y)
+#
+# elif y>x:
+#     print(y,"Y greater than x", x)
+#
+# else:
+#     print(x,y,"both x and y equal")
 
-elif userinput>cn:
-    print(cn)
-    print("user input greter than cn no")
+
+
+day=int(input("enter no. for day"))
+
+if day==1:
+    print("Sunday")
+
+elif day==2:
+    print("Monday")
+
+elif day==3:
+    print("Tuesday")
+
+elif day==4:
+    print("Wednesday")
+
+elif day==5:
+    print("Thursday")
+
+elif day==6:
+    print("Friday")
+
+elif day==7:
+    print("Saturday")
 
 else:
-    print("both no. are equal")
-
+    print("Wrong day")
